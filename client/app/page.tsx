5bb05a7dc008c7d6d89963506a8070f56beeb69a
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-27 23:15:18 +0530

    refactor

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,7 +4,7 @@ import { Features } from '@/components/Features';
 import { CTA } from '@/components/Cta';
 import { Footer } from '@/components/Footer';
 
-export default function HomePage() {
+export default function Home() {
   return (
     <div className="bg-background min-h-screen">
       <Navbar />
