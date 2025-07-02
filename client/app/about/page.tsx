8904df5958e103401ac0296dc7d277c2cfa6a154
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-02 23:44:00 +0530

    about page added

---
+++ client/app/about/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import {motion} from 'motion/react'
+
+const About = () => {
+    return (
+        <motion.div className='flex flex-col lg:p-10 items-center justify-center'>
+            <h1 className='lg:text-7xl text-2xl font-semibold'>About Us</h1>
+        </motion.div>
+    )
+}
+
+export default About
