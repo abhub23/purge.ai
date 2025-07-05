831fca48b3fe397df537aced7fd6f4d8ce3bf6db
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-05 23:52:21 +0530

    refactor

--- client/components/auth/Signin.tsx
+++ client/components/auth/Signin.tsx
@@ -3,16 +3,18 @@
 import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
 import { useSignin } from '@/store/AuthStates';
 import GoogleButton from './Googlebutton';
+import { GooglesignIn } from '@/lib/client-auth';
 
 const Signin = () => {
   const { isOpen, setOpen } = useSignin();
-
   return (
     <>
       <Dialog open={isOpen} onOpenChange={setOpen}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogTitle>Sign In with Google</DialogTitle>
-          <GoogleButton />
+          <GoogleButton
+            onClick={GooglesignIn}
+          />
         </DialogContent>
       </Dialog>
     </>
