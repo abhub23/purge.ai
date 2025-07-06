b5cab987be8c4168e7f4ecc476b9e0cf7b9afca7
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-06 23:40:05 +0530

    feat: entry of backend routes

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,30 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import authRouter from './routes/signin';
+import chatRouter from './routes/chat';
+import { toNodeHandler } from 'better-auth/node'
+import { auth } from './lib/auth'
+
 const Port = 4000;
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-  }),
-);
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
+app.use(helmet())
+
+app.all('/api/auth/*splat', toNodeHandler(auth))
 app.use(express.json());
 
 app.get("/", (_, res) => {
   res.json({ message: "Server is alive" });
 });
+
+app.use('/api', authRouter)
+
+app.use('/api', chatRouter)
 
 if (process.env.VERCEL !== "1") {
   app.listen(Port, () => {
