8511f46cd785088ee86a37ea01da83b35cdbb78d
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-07 23:53:21 +0530

    prompt field added, let the users choose the account

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -2,16 +2,19 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./config";
- 
+
 const prisma = new PrismaClient();
+
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    socialProviders:{
-        google: {
-            clientId: GOOGLE_CLIENT_ID, 
-            clientSecret: GOOGLE_CLIENT_SECRET, 
-        }
-    }
+  trustedOrigins: ['http://localhost:3000'],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    },
+  },
 });
