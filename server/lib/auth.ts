f043c5763922c57a95b25e85447fbfeaf7baf9cd
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-08 23:59:13 +0530

    refactor with prisma global

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,9 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./prisma";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./config";
 
-const prisma = new PrismaClient();
 
 export const auth = betterAuth({
   trustedOrigins: ['http://localhost:3000'],
@@ -14,7 +13,7 @@ export const auth = betterAuth({
     google: {
       prompt: "select_account",
       clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientSecret: GOOGLE_CLIENT_SECRET
     },
   },
 });
