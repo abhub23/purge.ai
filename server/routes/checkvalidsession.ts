e375bb2c3541d4cf439b3ccf215aa00548a92bca
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-12 23:55:04 +0530

    feat: added a valid session route to check the session from prisma and return a bool value and first name of user

--- server/routes/checkvalidsession.ts
+++ server/routes/checkvalidsession.ts
@@ -3,40 +3,52 @@ import type { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 const router = express.Router()
 
-router.get('/checkvalidsession', async (req: Request, res: any) => {
-    const cookie = req.cookies['better-auth.session_token'];
+router.get('/', async (req: Request, res: any) => {
+  const fullCookie = req.cookies['better-auth.session_token'];
 
+  if (!fullCookie) {
+    return res.json({
+      success: false,
+      message: 'Cookie not found by the server'
+    });
+  }
 
-    if (!cookie) {
-        res.status(401).json({
-            message: 'Cookie not found my the server'
-        })
-        return
+  try {
+    const cookie = fullCookie.split('.')[0];
+
+    const valid = await prisma.session.findUnique({
+      where: { token: cookie },
+      include: {
+        user: {
+          select: {
+            name: true
+          },
+        },
+      },
+    });
+
+    const name = valid?.user.name.split(' ')[0]
+
+    if (!valid) {
+      return res.status(404).json({
+        success: false,
+        message: 'Session not found'
+      });
     }
 
-    try {
-        const valid = await prisma.session.findUnique({
-            where: { token: cookie }
-        })
-        console.log('valid:   ', valid)
-        if (!valid) {
-            res.status(404).json({
-                success: false,
-                message: 'Session not found'
-            })
-            return
-        }
+    return res.status(200).json({
+      success: true,
+      message: 'Valid Session boy',
+      name
+    });
 
-        return res.status(200).json({
-            success: true,
-            message: 'Valid Session token'
-        })
-
-    } catch (error) {
-        console.error('Error occured finding session', error)
-        return
-    }
-
-})
+  } catch (error) {
+    console.error('Error occurred while finding session', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Server error while validating session'
+    });
+  }
+});
 
 export default router
