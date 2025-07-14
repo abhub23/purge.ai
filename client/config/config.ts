8caa6b19d4fd2524a64f83f190b6dccd95b47392
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-14 23:48:01 +0530

    added a get IP func to get the local ip address for allowing the mobile development

--- client/config/config.ts
+++ client/config/config.ts
@@ -1,1 +1,10 @@
+import os from 'os';
+
+function getLocalIP() {
+  const netInterface = os.networkInterfaces();
+  const IP = Object.values(netInterface).flat(1)[1]?.address as string;
+  return IP;
+}
+
+export const localIP = getLocalIP();
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:4000';
