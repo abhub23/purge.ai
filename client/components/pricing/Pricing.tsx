d950eb7fca1e24410b66c26d2c08cd24860f29b4
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-20 00:56:30 +0530

    added some motion animation to the card

--- client/components/pricing/Pricing.tsx
+++ client/components/pricing/Pricing.tsx
@@ -3,6 +3,7 @@
 import { PricingCard } from '@/components/pricing/PricingCard';
 import { PricingHeader } from '@/components/pricing/PricingHeader';
 import { PAYMENT_FREQUENCIES, TIERS } from '@/components/pricing/price';
+import {easeInOut, motion} from 'motion/react'
 import { useState } from 'react';
 
 export const Pricing = () => {
@@ -11,20 +12,23 @@ export const Pricing = () => {
   return (
     <section className="flex flex-col items-center gap-10 py-10">
       {/* Section Header */}
-      <PricingHeader
-        title="Plans and Pricing"
-        subtitle="Receive unlimited credits when you pay yearly, and save on your plan."
-        frequencies={PAYMENT_FREQUENCIES}
-        selectedFrequency={selectedPaymentFreq}
-        onFrequencyChange={setSelectedPaymentFreq}
-      />
+      <motion.div initial={{opacity: 0 , x: -30, filter: 'blur(4px)'}} animate={{opacity: 1 ,x: 0, filter: 'blur(0px)'}} transition={{duration: 0.5, ease: easeInOut}}>
+        <PricingHeader
+          title="Plans and Pricing"
+          subtitle="Receive unlimited credits when you pay yearly, and save on your plan."
+          frequencies={PAYMENT_FREQUENCIES}
+          selectedFrequency={selectedPaymentFreq}
+          onFrequencyChange={setSelectedPaymentFreq}
+        />
+      </motion.div>
+
 
       {/* Pricing Cards */}
-      <div className="grid w-full max-w-6xl gap-6 sm:grid-cols-2 xl:grid-cols-4">
+      <motion.div initial={{opacity: 0 , x: 30, filter: 'blur(4px)'}} animate={{opacity: 1 ,x:0, filter: 'blur(0px)'}} transition={{duration: 0.5, ease: easeInOut}} className="grid w-full max-w-6xl gap-6 sm:grid-cols-2 xl:grid-cols-4">
         {TIERS.map((tier, idx) => (
           <PricingCard key={idx} tier={tier} paymentFrequency={selectedPaymentFreq} />
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 };
