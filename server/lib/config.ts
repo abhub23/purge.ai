9984f459bca036d6e9eab88c75adb07beab1c7fb
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-20 23:54:40 +0530

    added razorpay id and secret

--- server/lib/config.ts
+++ server/lib/config.ts
@@ -6,4 +6,6 @@ export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
 export const BETTER_AUTH_SECRET = process.env.BETTER_AUTH_SECRET as string
 export const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL as string
 export const LLM = process.env.LLM as string
+export const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID as string
+export const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET as string
 export const FRONTEND_URL = process.env.FRONTEND_URL as string || 'http://localhost:3000'
