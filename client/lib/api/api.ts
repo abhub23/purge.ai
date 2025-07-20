15ef6255109286746f766989c2fec5e97ec59b1a
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-20 23:53:28 +0530

    refactoring

--- client/lib/api/api.ts
+++
@@ -1,6 +0,0 @@
-import api from "./axios";
-
-export const CheckvalidSession = async () => {
-  const response = await api.get('/api/checkvalidsession');
-  return response.data;
-};
--- client/lib/client-auth.ts
+++ client/lib/client-auth.ts
@@ -1,16 +1,16 @@
 import { createAuthClient } from 'better-auth/client';
+import { BACKEND_URL } from '@/config/config';
 
 const authClient = createAuthClient({
   //backend url (necessary when frontend and backend are on seperate ports)
-  baseURL: 'http://localhost:4000',
+  baseURL: BACKEND_URL,
   // this dont even have a callback URL property
 });
 
 export const GoogleSignIn = async () => {
   await authClient.signIn.social({
     provider: 'google',
-    callbackURL: 'http://localhost:3000', //redirects to this URL after successful sign in
-
+    callbackURL: ' http://localhost:3000', //redirects to this URL after successful sign in
   });
 };
 
