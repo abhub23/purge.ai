70ab0b21d915dfc9c14e7aeb7c53e5a927178ce0
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-25 03:23:31 +0530

    set the position based on pathname

--- client/components/ui/dialog.tsx
+++ client/components/ui/dialog.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { XIcon } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 import { cn } from '@/lib/utils';
 
@@ -46,13 +47,15 @@ function DialogContent({
 }: React.ComponentProps<typeof DialogPrimitive.Content> & {
   showCloseButton?: boolean;
 }) {
+  const pathname = usePathname()
+  
   return (
     <DialogPortal data-slot="dialog-portal">
       <DialogOverlay />
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          'bg-card/20 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[48%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] justify-center gap-4 rounded-lg border p-6 shadow-lg backdrop-blur-sm duration-200 sm:max-w-lg lg:top-[50%]',
+          `bg-card/5  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[48%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] justify-center gap-4 rounded-lg border p-6 shadow-lg backdrop-blur-xs duration-200 sm:max-w-lg ${pathname == '/pricing' ? 'lg:top-[45%]' : 'lg:top-[42%]' }`,
           className
         )}
         {...props}
