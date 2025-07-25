36b98d4c0957676ca46d547dedfbe17635b3adc6
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-25 23:32:42 +0530

    some refactor

--- client/components/main/Footer.tsx
+++ client/components/main/Footer.tsx
@@ -26,28 +26,28 @@ export const Footer = () => {
             <div className="flex h-5 w-5 space-x-4">
               <Link
                 href="https://github.com/abhub23/purge.ai"
-                className="text-muted-foreground hover:text-foreground transition-colors"
+                className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 target="_blank"
               >
                 <Github className="h-5 w-5" />
               </Link>
               <Link
                 href="https://x.com/abdullah_twt23"
-                className="text-muted-foreground hover:text-foreground transition-colors"
+                className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 target="_blank"
               >
                 <Twitter className="h-5 w-5" />
               </Link>
               <Link
                 href="https://www.linkedin.com/in/abdullah-mukri-84a56b220/"
-                className="text-muted-foreground hover:text-foreground transition-colors"
+                className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 target="_blank"
               >
                 <Linkedin className="h-5 w-5" />
               </Link>
               <Link
                 href="https://mail.google.com/mail/?view=cm&to=abdullahmukri25@gmail.com"
-                className="text-muted-foreground hover:text-foreground transition-colors"
+                className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 target="_blank"
               >
                 <Mail className="h-5 w-5" />
@@ -61,35 +61,14 @@ export const Footer = () => {
             <ul className="space-y-2 text-sm">
               <li>
                 <Link
-                  href="/features"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
-                >
-                  Features
-                </Link>
-              </li>
-              <li>
-                <Link
                   href="/pricing"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
+                  className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 >
                   Pricing
                 </Link>
               </li>
-              <li>
-                <Link
-                  href="/changelog"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
-                >
-                  Changelog
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/roadmap"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
-                >
-                  Roadmap
-                </Link>
+              <li className='text-muted-foreground hover:text-foreground/85 transition-colors cursor-pointer' onClick={() => window.open('https:abdullahtech.dev', '_blank')}>
+                Developer/Creator
               </li>
             </ul>
           </div>
@@ -101,21 +80,13 @@ export const Footer = () => {
               <li>
                 <Link
                   href="/docs"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
+                  className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 >
                   Documentation
                 </Link>
               </li>
-              <li>
-                <Link
-                  href="/blog"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
-                >
-                  Blog
-                </Link>
-              </li>
               <li
-                className="text-muted-foreground hover:text-foreground transition-colors hover:cursor-pointer"
+                className="text-muted-foreground hover:text-foreground/85 transition-colors hover:cursor-pointer"
                 onClick={ScrollY}
               >
                 Tutorial
@@ -123,7 +94,7 @@ export const Footer = () => {
               <li>
                 <Link
                   href="https://mail.google.com/mail/?view=cm&to=abdullahmukri25@gmail.com&su=PurgeAI%20Support%20Team"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
+                  className="text-muted-foreground hover:text-foreground/85 transition-colors"
                   target="_blank"
                 >
                   Support
@@ -139,7 +110,7 @@ export const Footer = () => {
               <li>
                 <Link
                   href="/about"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
+                  className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 >
                   About
                 </Link>
@@ -147,49 +118,24 @@ export const Footer = () => {
 
               <li>
                 <Link
-                  href="/contact"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
+                  href="https://mail.google.com/mail/?view=cm&to=abdullahmukri25@gmail.com&su=PurgeAI%20Contact"
+                  target='_blank'
+                  className="text-muted-foreground hover:text-foreground/85 transition-colors"
                 >
                   Contact
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/privacypolicy"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
-                >
-                  Privacy Policy
                 </Link>
               </li>
             </ul>
           </div>
         </div>
-
-        {/* Bottom Section */}
-        <div className="mt-6 flex flex-col items-center justify-between space-y-2 border-t pt-4 md:space-y-2 lg:mx-auto">
-          <p className="text-muted-foreground text-sm">© 2025 PurgeAI. All rights reserved.</p>
-          <div className="flex items-center gap-x-[10px] text-sm md:gap-6">
-            <Link
-              href="/terms"
-              className="text-muted-foreground hover:text-foreground transition-colors"
-            >
-              Terms of Service
-            </Link>
-            <Link
-              href="/privacypolicy"
-              className="text-muted-foreground hover:text-foreground transition-colors"
-            >
-              Privacy Policy
-            </Link>
-            <Link
-              href="/cookiepolicy"
-              className="text-muted-foreground hover:text-foreground transition-colors"
-            >
-              Cookie Policy
-            </Link>
-          </div>
-        </div>
       </div>
+
+      <p className="text-muted-foreground flex items-center justify-center border-t text-[12px] h-13 lg:h-16 lg:text-[14px]">
+        © 2025 PurgeAI. All rights reserved.
+        <Link href={'/t&c'} className="hover:text-foreground/75 transition-colors ml-2 underline">
+          Terms & Conditions{' '}
+        </Link>
+      </p>
     </footer>
   );
 };
