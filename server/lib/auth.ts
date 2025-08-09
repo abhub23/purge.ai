419af788939aeaca3f1c37efca32712a37baeaef
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-09 23:23:02 +0530

    added a condition on user creation the plan modal will be populated

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,8 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./prisma";
+import { prisma } from "../lib/prisma";
 import { FRONTEND_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./config";
 
+const Week_in_Ms = 7 * 24 * 60 * 60 * 1000
+const Creds = 5
 
 export const auth = betterAuth({
   trustedOrigins: [FRONTEND_URL],
@@ -16,4 +18,21 @@ export const auth = betterAuth({
       clientSecret: GOOGLE_CLIENT_SECRET
     },
   },
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          await prisma.plan.create({
+            data: {
+              userId: user.id,
+              credits: 5,
+              expiresAt: new Date(Date.now() + Week_in_Ms),
+            }
+          });
+        },
+      }
+    }
+  }
 });
+
+
