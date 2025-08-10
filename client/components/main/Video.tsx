8688541d124c56b4149725b549b39759f34ba373
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-11 00:07:46 +0530

    video is yet to set else config is set

---
+++ client/components/main/Video.tsx
@@ -0,0 +1,17 @@
+const Video = () => {
+  return (
+    <video
+      autoPlay
+      muted
+      loop
+      playsInline
+      controls={false}
+      className="pointer-events-none h-auto lg:w-[600px]"
+    >
+      <source src="/video.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
+
+export default Video;
