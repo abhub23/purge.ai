3b2d06d769eed2fd5882e352276daf3b2d7084f9
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-18 00:55:35 +0530

    added a shimmering effect to the loader

--- client/components/ai-elements/loader.tsx
+++ client/components/ai-elements/loader.tsx
@@ -1,15 +1,14 @@
 import { Brain } from 'lucide-react';
-import { motion } from 'motion/react';
+import { TextShimmer } from '../ui/text-shimmer';
 
-export const Loader = () => (
-  <motion.div
-    className="inline-block text-neutral-900 select-none dark:text-neutral-100"
-    animate={{ y: [0, -6, 0] }}
-    transition={{ duration: 1, repeat: Infinity, ease: 'easeInOut' }}
-  >
-    <div className="flex items-center">
-      <Brain className="size-6 px-[4px]" />
-      Thinking<span className="px-1">...</span>
+export const Loader = () => {
+  return (
+    <div className='flex items-center'>
+      <Brain className="size-6 px-[4px] " />
+      <TextShimmer>
+        Thinking
+      </TextShimmer>
     </div>
-  </motion.div>
-);
+  )
+
+}
