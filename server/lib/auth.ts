b0e2631c5f827db8849f79fa3ef678ed766eb871
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-21 07:02:56 +0530

    added constants to the constants folder

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -2,9 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "../lib/prisma";
 import { FRONTEND_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./config";
-
-const Week_in_Ms = 7 * 24 * 60 * 60 * 1000
-const Creds = 5
+import { InitialCredits, Week_in_Ms } from "../constants/constants";
 
 export const auth = betterAuth({
   trustedOrigins: [FRONTEND_URL],
@@ -25,7 +23,7 @@ export const auth = betterAuth({
           await prisma.plan.create({
             data: {
               userId: user.id,
-              credits: 5,
+              credits: InitialCredits,
               expiresAt: new Date(Date.now() + Week_in_Ms),
             }
           });
