1f3af458fb09889e48ba58634f65173113148819
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-21 07:15:46 +0530

    added trust proxy middleware for getting user headers and added more routes

--- server/index.ts
+++ server/index.ts
@@ -2,22 +2,26 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import chatRouter from './routes/chat';
+import chat from './routes/chat';
 import checkvalidsession from './routes/checkvalidsession'
+import orderpay from './routes/orderpay'
 import { toNodeHandler } from 'better-auth/node'
 import { auth } from './lib/auth'
 import { FRONTEND_URL } from "./lib/config";
+import verifypayment from './routes/verifypayment'
 
 const Port = 4000;
 const app = express();
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: [FRONTEND_URL],
   credentials: true
 }));
 
 app.use(helmet())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', true); // Tells Express to trust x-forwarded-for in req header
+
 
 app.all('/api/auth/*splat', toNodeHandler(auth))
 app.use(express.json());
@@ -27,8 +31,10 @@ app.get("/", (_, res) => {
 });
 
 
-app.use('/api/chat', chatRouter)
+app.use('/api/chat', chat)
 app.use('/api/checkvalidsession', checkvalidsession)
+app.use('/api/orderpay', orderpay)
+app.use('/api/verifypayment', verifypayment)
 
 if (process.env.VERCEL !== "1") {
   app.listen(Port, () => {
