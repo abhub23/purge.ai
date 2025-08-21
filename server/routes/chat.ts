4914a8994e8dd6b76842e450beadfd7e0f2f7615
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-21 06:58:57 +0530

    added chat router to tall with llms

--- server/routes/chat.ts
+++ server/routes/chat.ts
@@ -1,9 +1,45 @@
-import express from "express";
+import express, { type Request } from 'express';
+import { streamText, convertToModelMessages, UIMessage } from 'ai';
+import { google } from '@ai-sdk/google';
+import { checkCredits } from '../middlewares/checkCredits';
+import axios from 'axios';
+import { rejectionMessages, githubPrUrlRegex, Prompt } from '../constants/constants';
+import { LLM_MODEL } from '../lib/config';
 
-const router = express.Router()
+const router = express.Router();
+router.use(checkCredits)
 
-router.get('/chat', (req, res) => {
-    res.json({message: 'signin'})
-})
+type ChatReqBody = {
+  text: string;
+  messages: UIMessage[];
+}
 
-export default router
+router.post('/', async (req: any, res: any) => {
+
+  const { text, messages }: ChatReqBody = req.body;
+  const userId = req.authUser?.id || req.guestUser?.id
+
+  if (!githubPrUrlRegex.test(text ?? "")) {
+    const pick = Math.floor(Math.random() * 20)
+    return res.status(200).send(rejectionMessages[pick]);
+  }
+
+  try {
+    const RawDiff = await axios.get(`${text}.diff`)
+    let systemPrompt = Prompt(RawDiff.data)
+
+    const result = streamText({
+      model: google(LLM_MODEL),
+      messages: convertToModelMessages(messages),
+      system: systemPrompt
+    });
+
+    result.pipeTextStreamToResponse(res);
+
+  } catch (error) {
+    console.error('Error in /api/chat:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+export default router;
