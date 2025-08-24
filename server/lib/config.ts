cb9a2e1e53ede211dcf2662e4ace735c018d7663
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-24 13:12:40 +0530

    some conditional change in config and refactor

--- server/lib/config.ts
+++ server/lib/config.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
 export const BETTER_AUTH_SECRET = process.env.BETTER_AUTH_SECRET as string
@@ -8,4 +10,4 @@ export const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL as string
 export const LLM_MODEL = process.env.LLM_MODEL as string
 export const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID as string
 export const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET as string
-export const FRONTEND_URL = process.env.FRONTEND_URL as string || 'http://localhost:3000'
+export const FRONTEND_URL = isProd ? process.env.FRONTEND_URL as string : 'http://localhost:3000'
--- server/routes/orderpay.ts
+++ server/routes/orderpay.ts
@@ -8,17 +8,17 @@ import { OrderSchema } from "../schemas/payment.schema";
 const router = express.Router()
 
 router.post('/', validSession, async (req: Request, res: any) => {
-    const result = OrderSchema.safeParse(req.body)
+    const parsedBody = OrderSchema.safeParse(req.body)
 
-    if(!result.success){
+    if(!parsedBody.success){
         return res.status(400).json({
             success: false,
-            msg: result.error.issues[0].message
+            message: parsedBody.error.issues[0].message
         })
     }
     
     try {
-        const { plan_id, price } = result.data
+        const { price } = parsedBody.data
         const options = {
             amount: price * 86 * 100, // USD -> INR paise
             currency: 'INR',
