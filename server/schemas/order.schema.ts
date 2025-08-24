1e7c6b444af43b570bec885b6198ca80718ae906
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-24 13:01:07 +0530

    deleted

--- server/schemas/order.schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const OrderSchema = z.object({
-  plan_id: z.literal('pro', { message: "Plan must be 'pro' only" }),
-  price: z.number("price must be a number").refine(val => [6,8].includes(val), {message: 'The price is either $6 or $8'} )
-});
