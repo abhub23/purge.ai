6ffcb23652cf419974afc9ad7b3b0c4bd5a69a69
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-27 21:14:40 +0530

    added export

--- server/lib/config.ts
+++ server/lib/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
-const isProd = process.env.NODE_ENV === 'production';
+export const isProd = process.env.NODE_ENV === 'production';
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
