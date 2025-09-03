fce7f60a1062ca5fba82adc161418bad8f281b81
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-09-03 23:53:50 +0530

    refactor

--- server/routes/chat.ts
+++ server/routes/chat.ts
@@ -1,4 +1,4 @@
-import express, { type Request } from 'express';
+import { type Request, Router } from 'express';
 import { streamText, convertToModelMessages, UIMessage } from 'ai';
 import { google } from '@ai-sdk/google';
 import { checkCredits } from '../middlewares/checkCredits';
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { rejectionMessages, githubPrUrlRegex, Prompt } from '../constants/constants';
 import { LLM_MODEL } from '../lib/config';
 
-const router = express.Router();
+const router = Router();
 router.use(checkCredits)
 
 type ChatReqBody = {
@@ -19,7 +19,7 @@ router.post('/', async (req: any, res: any) => {
   const { text, messages }: ChatReqBody = req.body;
   const userId = req.authUser?.id || req.guestUser?.id
 
-  if (!githubPrUrlRegex.test(text ?? "")) {
+  if (!githubPrUrlRegex.test(text)) {
     const pick = Math.floor(Math.random() * 20)
     return res.status(200).send(rejectionMessages[pick]);
   }
