64524ce8f4415bfa1e4f651810175cfd847aa8a9
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-10-10 19:13:03 +0530

    added Inview animations to features

--- client/components/main/Features.tsx
+++ client/components/main/Features.tsx
@@ -1,27 +1,29 @@
+'use client' 
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { features } from '@/constants/features';
+import {motion} from 'motion/react'
+import { Inview } from '@/animations/Animation';
 
 export const Features = () => {
   return (
     <section className='mt-[-120px] py-24 md:py-32'>
-      <div className='mx-auto flex max-w-[980px] flex-col items-center space-y-4 text-center'>
+      <motion.div {...Inview} className='mx-auto flex max-w-[980px] flex-col items-center space-y-4 text-center'>
         <Badge variant='secondary' className='border py-1'>
           Features
         </Badge>
-        <h2 className='mx-2 text-3xl leading-tight font-bold tracking-tighter lg:text-5xl'>
+        <motion.h2 className='mx-2 text-3xl leading-tight font-bold tracking-tighter lg:text-5xl'>
           Everything you need to know for building amazing SaaS products
-        </h2>
+        </motion.h2>
         <p className='text-muted-foreground lg:-w-[800px] mx-2 text-[15px] lg:text-lg'>
           Our platform provides all the tools and features you need to create exceptional Products
         </p>
-      </div>
+      </motion.div>
       <div className='mx-2 mt-16 grid max-w-[1200px] grid-cols-1 gap-6 md:grid-cols-2 lg:mx-auto lg:grid-cols-3'>
         {features.map((feature, index) => (
-          <Card
-            key={index}
-            className='relative overflow-hidden p-5 transition-all delay-50 hover:scale-101 hover:shadow-lg'
-          >
+          <motion.div {...Inview} key={index}>
+          <Card className='relative overflow-hidden p-5 transition-all delay-50 hover:scale-101 hover:shadow-lg'>
             <CardHeader>
               <div className='flex items-center space-x-2'>
                 <div className='bg-muted flex h-10 w-10 items-center justify-center rounded-lg border'>
@@ -35,6 +37,7 @@ export const Features = () => {
               <CardDescription className='w-full text-base'>{feature.desc}</CardDescription>
             </CardContent>
           </Card>
+          </motion.div>
         ))}
       </div>
     </section>
