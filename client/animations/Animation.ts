cc2d1fe7bf848775b9c16896cf499c334e4194c1
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-10-10 19:12:27 +0530

    added Inview animations

--- client/animations/Animation.ts
+++ client/animations/Animation.ts
@@ -1,14 +1,6 @@
 export const Animation: any = {
-  initial: {
-    opacity: 0,
-    y: 20,
-    filter: 'blur(10px)',
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
-    filter: 'blur(0px)',
-  },
+  initial: { opacity: 0, y: 20, filter: 'blur(10px)'},
+  animate: { opacity: 1, y: 0, filter: 'blur(0px)'}
 };
 
 export const Transition = (x: number) => {
@@ -18,3 +10,10 @@ export const Transition = (x: number) => {
     delay: x,
   };
 };
+
+export const Inview = {
+  initial: { opacity: 0, y: 50 },
+  whileInView: { opacity: 1, y: 0 },
+  transition: { duration: 0.6 },
+  viewport: { amount: 0.2, once: true } 
+}
