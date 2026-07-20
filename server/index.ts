244b6d5dbbf367b912248e5f9480fea312edcece
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-07-20 12:53:21 +0530

    index file wired up

--- server/index.ts
+++ server/index.ts
@@ -3,14 +3,15 @@ import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import chat from './modules/chat/chat.routes.js';
-import checkvalidsession from './routes/checkvalidsession.js'
-import orderpay from './routes/orderpay.js'
+import checkvalidsession from './modules/check-valid-session/check-valid-session.routes.js'
+import orderpay from './modules/order-pay/order-pay.routes.js'
+import verifyPay from './modules/verify-payment/verify-payment.routes.js'
 import { toNodeHandler } from 'better-auth/node'
 import { auth } from './lib/auth.js'
 import { FRONTEND_URL } from "./lib/config.js";
-import verifypayment from './routes/verifypayment.js'
+// import { errorHandler } from './middlewares/errorHandler.js';
 
-const Port = 4000;
+const port = 4000;
 const app = express();
 app.use(cors({
   origin: [FRONTEND_URL],
@@ -27,7 +28,7 @@ app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json());
 
 app.get("/health", (_, res) => {
-  res.status(200).json({ message: "Server is healthy" });
+  res.status(200).json({ message: "server is healthy" });
 });
 
 app.get("/", (_, res) => {
@@ -38,11 +39,13 @@ app.get("/", (_, res) => {
 app.use('/api/chat', chat)
 app.use('/api/checkvalidsession', checkvalidsession)
 app.use('/api/orderpay', orderpay)
-app.use('/api/verifypayment', verifypayment)
+app.use('/api/verifypayment', verifyPay)
+
+// app.use(errorHandler);
 
 if (process.env.VERCEL !== "1") {
-  app.listen(Port, () => {
-    console.log(`Local server running on ${Port}`);
+  app.listen(port, () => {
+    console.log(`Local server running on ${port}`);
   });
 }
 
